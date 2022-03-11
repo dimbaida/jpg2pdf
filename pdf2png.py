f35e43f9e03f360320557426e7b9d0f994c0ed39
Dim Baidachnyi <dimbaida@gmail.com>
2022-03-11 17:19:08 +0200

    added poppler and requirements

--- pdf2png.py
+++ pdf2png.py
@@ -1,11 +1,14 @@
 from pdf2image import convert_from_path
 import os
 
-path_pdf = r"D:\Dropbox\Documents\Organisations\4AGames\Offer_Letter_Baidachnyi.pdf"
-path_png = r"D:\Dropbox\Documents\Organisations\4AGames\png"
+poppler_path = os.path.join(os.getcwd(), r'poppler-0.68.0\bin')
+
+path_pdf = r"C:\Users\user\Desktop\1.pdf"
+path_png = r"C:\Users\user\Desktop\png"
+
 if not os.path.exists(path_png):
     os.mkdir(path_png)
 
-pages = convert_from_path(path_pdf, poppler_path=r"C:\Program Files\poppler-0.68.0\bin")
+pages = convert_from_path(path_pdf, poppler_path=poppler_path)
 for i, page in enumerate(pages):
     page.save(os.path.join(path_png, f'page_{str(i+1).zfill(2)}.png'))
